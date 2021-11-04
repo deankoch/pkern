7fb6346535b81656240edbd3c661b870fdc073a4
deankoch <deankoch@gmail.com>
2021-11-03 19:49:21 -0600

    renamed variables for consistency with other functions, fixed CRS handling

--- R/pkern_raster.R
+++ R/pkern_raster.R
@@ -27,16 +27,15 @@ pkern_checkRaster = function()
 #' The return value depends on argument `what`:
 #'
 #' "gdim": returns integer vector `c(ny, nx)`, the number of y and x grid lines
-#' "ds": returns `c(dy, dx)`, the x and y distances between grid lines (ie the resolution)
-#' "crs": returns the CRS code for the raster (if any)
-#' "yx": returns `list(y, x)`, a list of vectors containing the y and x grid line positions
-#' "values": returns a vector containing the raster data in column-vectorized order
-#' "all": returns a named list containing the above three objects
+#' "gres": returns `c(dy, dx)`, the x and y distances between grid lines (ie the resolution)
+#' "gyx": returns `list(y, x)`, a list of vectors containing the y and x grid line positions
+#' "gval": returns a vector containing the raster data in column-vectorized order
+#' "all": returns a named list containing the above four objects
 #'
 #' @param r a RasterLayer to vectorize
-#' @param what character, one of: 'gdim', 'ds', 'u', 'crs', 'yx', 'values' or 'all'
+#' @param what character, one of: 'gdim', 'gres', 'crs', 'gyx', 'gval' or 'all'
 #'
-#' @return For default `what=='all'`, a named list containing 6 objects (see details)
+#' @return For default `what=='all'`, a named list containing 4 objects (see details)
 #' @export
 #'
 #' @examples
@@ -44,7 +43,7 @@ pkern_checkRaster = function()
 #' r.in = system.file('external/rlogo.grd', package='raster') |> raster::raster(band=1)
 #' pkern.in = pkern_fromRaster(r.in)
 #' pkern_plot(pkern.in)
-#' pkern.in$values |> matrix(pkern.in$gdim) |> pkern_plot()
+#' pkern.in$gval |> matrix(pkern.in$gdim) |> pkern_plot()
 #' }
 pkern_fromRaster = function(r, what='all')
 {
@@ -52,59 +51,44 @@ pkern_fromRaster = function(r, what='all')
   pkern_checkRaster()
 
   # extract dimensions and return if requested
-  gdim = dim(r)[1:2] |> stats::setNames(c('i', 'j'))
+  gdim = dim(r)[1:2] |> stats::setNames(c('y', 'x'))
   if( what == 'gdim' ) return(gdim)
 
   # notice return from raster::res is in reverse order (dx, dy)
-  ds = raster::res(r)[2:1] |> stats::setNames(c('y', 'x'))
-  if( what == 'ds' ) return(ds)
+  gres = raster::res(r)[2:1] |> stats::setNames(c('y', 'x'))
+  if( what == 'gres' ) return(gres)
 
   # extract CRS string if available
-  crs.r = ifelse(requireNamespace('rgdal', quietly=TRUE), rgdal::CRSargs(raster::crs(r)), NA)
-  if( what == 'crs' ) return(crs.r)
+  gcrs = raster::crs(r)
+  if( what == 'crs' ) return(gcrs)
 
-  # attempt to parse resolution units from CRS string
-  u = NA
-  regex.units = '+units\\='
-  if( grepl(regex.units, crs.r) )
+  # extract grid line positions
+  if( what != 'gval' )
   {
-    crs.split = strsplit(crs.r, regex.units)[[1]]
-    u = substr(crs.split[ length(crs.split) ], 1, 1)
-  }
-
-  # return units if requested
-  if( what == 'u' ) return(u)
-
-  # extract grid line positions (as needed)
-  if( what != 'values' )
-  {
-    yasc = sort( raster::yFromRow(r, seq(gdim[1]) ) )
-    xasc = sort( raster::xFromCol(r, seq(gdim[2]) ) )
-    yx = list(y=yasc, x=xasc)
+    gy = sort(raster::yFromRow(r, seq(gdim['y'])))
+    gx = sort(raster::xFromCol(r, seq(gdim['x'])))
+    gyx = list(y=gy, x=gx)
 
     # finish grid line mode
-    if( what == 'yx' ) return(yx)
+    if( what == 'gyx' ) return(gyx)
   }
 
-  # indexing vector to get column-vectorized version of values
-  cvec.idx = pkern_r2c(gdim)
-
-  # extract vectorized data and finish
-  rvec = raster::getValues(r)[cvec.idx]
-  if(what == 'values') return(rvec)
-  return( list(gdim=gdim, ds=ds, crs=crs.r, u=u, yx=yx, values=rvec) )
+  # extract data in column-vectorized order and finish
+  gval = raster::getValues(r)[ pkern_r2c(gdim) ]
+  if(what == 'gval') return(gval)
+  return( list(gdim=gdim, gres=gres, crs=gcrs, gyx=gyx, gval=gval) )
 }
 
 #' Convert column-vectorized grid to RasterLayer
 #'
-#' @param rvec either the column-vectorized data or a list containing it (in element "values")
-#' @param gdim c(ni, nj), the number rows and columns in the full grid
+#' @param g either the column-vectorized data or a list containing it (in element "gval")
+#' @param gdim c(ny, nx), the number rows and columns in the full grid
 #' @param template optional RasterLayer to use as template (for setting crs, resolution etc)
 #'
 #' Produces a RasterLayer from a vector or matrix. This essentially a wrapper for various
 #' `raster::raster` calls where the input data is in column-vectorized form.
 #'
-#' `rvec` can either be a matrix (in which case `gdim` can be omitted) or its vectorized
+#' `g` can either be a matrix (in which case `gdim` can be omitted) or its vectorized
 #' equivalent, or a list containing elements returned by `pkern_fromRaster`. If the list
 #' has an element named "gdim", it replaces any argument supplied to `gdim`. Other elements
 #' of the list (eg 'crs') are passed on to the RasterLayer construction call as needed.
@@ -121,29 +105,36 @@ pkern_fromRaster = function(r, what='all')
 #' print(pkern_toRaster(pkern.in))
 #' print(r.in)
 #' }
-pkern_toRaster = function(rvec=NULL, gdim=NULL, template=NULL)
+pkern_toRaster = function(g=NULL, gdim=NULL, template=NULL)
 {
   # stop with an error message if raster package is unavailable
   pkern_checkRaster()
 
   # flags for different types of raster calls
   has.template = !is.null(template)
-  has.properties = is.list(rvec)
+  has.properties = is.list(g)
 
   # assign dimensions from matrix input
-  if( is.null(gdim) & is.matrix(rvec) ) gdim = rev(dim(rvec))
+  if( is.null(gdim) & is.matrix(g) ) gdim = rev(dim(g))
 
   # unpack list input
-  if( is.list(rvec) )
+  gres = gyx = NULL
+  rcrs = ''
+  if( has.properties )
   {
-    # unpack the data (overwrites argument to `gdim`)
-    if( !is.null( rvec[['gdim']] ) ) gdim = rvec[['gdim']]
-    rcrs = ifelse( is.null(rvec[['crs']]), '', rvec[['crs']])
-    ds = rvec[['ds']]
-    yx = rvec[['yx']]
+    # check for expected elements
+    # nm.expected = c('gdim', 'gres', 'gyx', 'gval')
+    # err.nm = paste('g must contain named elements', paste(nm.expected, collapse=', '))
+    # if( !all( nm.expected %in% names(g) )) stop(err.nm)
 
-    # rvec should be a matrix or vector from here on
-    rvec = rvec[['values']]
+    # unpack the data (overwrites argument to `gdim`)
+    if( !is.null( g[['gdim']] ) ) gdim = g[['gdim']]
+    if( !is.null( g[['crs']] ) ) rcrs = g[['crs']]
+    gres = g[['gres']]
+    gyx = g[['gyx']]
+
+    # g should be a matrix or vector from here on
+    g = g[['gval']]
   }
 
   # extract `gdim` from template when supplied
@@ -155,33 +146,33 @@ pkern_toRaster = function(rvec=NULL, gdim=NULL, template=NULL)
   }
 
   # matricize data as needed
-  has.values = !is.null(rvec)
-  if( (!is.matrix(rvec)) & has.values ) rvec = matrix(rvec, gdim)
+  has.values = !is.null(g)
+  if( (!is.matrix(g)) & has.values ) g = matrix(g, gdim)
 
   # the raster call is simple with a template
-  if( (has.template | !has.properties) & has.values ) return(raster::raster(rvec, template=template))
+  if( (has.template | !has.properties) & has.values ) return(raster::raster(g, template=template))
 
   # compute dimensional stuff when properties supplied
   if( has.properties )
   {
-    # set defaults for ds and yx
-    if( is.null(ds) ) ds = c(1,1)
-    if( is.null(yx) ) yx = Map(\(d,s) c(1, d)-(s/2), d=gdim, s=ds)
+    # set defaults for gres and gyx
+    if( is.null(gres) ) gres = c(1,1)
+    if( is.null(gyx) ) gyx = Map(\(d,s) c(1,d)-(s/2), d=gdim, s=gres)
 
     # extract coordinate "boundaries" as required by raster
-    yxb = Map(\(g,s) range(s*g) + (c(-1, 1) * s/2), g=yx, s=ds)
+    yxb = Map(\(g,s) range(g) + (c(-1,1) * s/2), g=gyx, s=gres)
 
     # without a template the call has separate arguments for the four extent points
     if( has.values )
     {
       # build the raster and return
-      return( raster::raster(rvec, yxb[[2]][1], yxb[[2]][2], yxb[[1]][1], yxb[[1]][2], crs=rcrs) )
+      return( raster::raster(g, yxb[[2]][1], yxb[[2]][2], yxb[[1]][1], yxb[[1]][2], crs=rcrs) )
     }
   }
 
   # without values we can use a simpler looking call by passing an extent object
   ext = raster::extent(do.call(c, yxb))
-  return( raster::raster(crs=rcrs, ext=ext, resolution=ds) )
+  return( raster::raster(crs=rcrs, ext=ext, resolution=gres) )
 }
 
 
